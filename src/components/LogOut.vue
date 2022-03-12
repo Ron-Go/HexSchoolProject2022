9d9023e224fd09b7fff29d7074c4ec23399a9769
Ron <hpleain@gmail.com>
2022-03-12 20:02:50 +0800

    LogOut.vue加入vue loading

--- src/components/LogOut.vue
+++ src/components/LogOut.vue
@@ -1,17 +1,21 @@
 <template lang="">
-<div>
+<div class="position-relative" style="height:90vh" ref="logoutDom">
 </div>
 </template>
 <script>
+import loading from '../mixins/MixinLoading';
+
 export default {
   mounted() {
     this.logout();
   },
   methods: {
     logout() {
+      this.onLoading(this.$refs.logoutDom);
       this.axios
         .post(`${process.env.VUE_APP_API}/logout`)
         .then(() => {
+          this.offLoading();
           // 跳頁到登入頁面
           this.$router.push('/front/login');
         })
@@ -20,6 +24,7 @@ export default {
         });
     },
   },
+  mixins: [loading],
 };
 </script>
 <style lang=""></style>
