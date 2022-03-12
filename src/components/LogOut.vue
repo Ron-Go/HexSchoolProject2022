a3f2e8553fd64441d4d9eef34b30018f0003d5fb
Ron <hpleain@gmail.com>
2022-03-12 20:08:31 +0800

    解決衝突後commit

--- src/components/LogOut.vue
+++ src/components/LogOut.vue
@@ -1,34 +1,21 @@
 <template lang="">
-<<<<<<< HEAD
-<div>
-</div>
-</template>
-<script>
-=======
 <div class="position-relative" style="height:90vh" ref="logoutDom">
 </div>
 </template>
 <script>
 import loading from '../mixins/MixinLoading';
 
->>>>>>> feature/front/login
 export default {
   mounted() {
     this.logout();
   },
   methods: {
     logout() {
-<<<<<<< HEAD
-      this.axios
-        .post(`${process.env.VUE_APP_API}/logout`)
-        .then(() => {
-=======
       this.onLoading(this.$refs.logoutDom);
       this.axios
         .post(`${process.env.VUE_APP_API}/logout`)
         .then(() => {
           this.offLoading();
->>>>>>> feature/front/login
           // 跳頁到登入頁面
           this.$router.push('/front/login');
         })
@@ -37,10 +24,7 @@ export default {
         });
     },
   },
-<<<<<<< HEAD
-=======
   mixins: [loading],
->>>>>>> feature/front/login
 };
 </script>
 <style lang=""></style>
